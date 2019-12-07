7a2a62040907b45a0b60d1dc8c2087c592a5fe8c
fbo17 <972579737@qq.com>
2019-12-07 13:28:43 +0800

    4

--- GettingStarted/scripts/events.js
+++ GettingStarted/scripts/events.js
@@ -193,3 +193,25 @@ function checkContextDiagram(){
     else
         alert(correct);
 }
+
+function checkProblemDiagram(){
+    var models = paper.model.attributes.cells.models;
+    var flag=false;
+    var cph=true;
+    var rph=true;
+    for(var i=0;i<models.length;i++){
+        if(models[i].attributes.type=="constraint.CustomLink"){
+            flag=true;
+            if(models[i].phenomenon == undefined || models[i].phenomenon.length==0) cph = false;
+        }
+
+        if(models[i].attributes.type=="reference.CustomLink"){
+            if(models[i].phenomenon == undefined || models[i].phenomenon.length==0) rph = false;
+        }
+    }
+    if(flag==false) alert("Please add at least one constraint!");
+    else if(cph==false) alert("Please add phenomenon on the constraint!");
+    else if(rph==false) alert("Please add phenomenon on the reference!")
+    else alert("Correct Requirement Diagram!");
+    return;
+}
